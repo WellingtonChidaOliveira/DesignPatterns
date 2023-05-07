ce285394c012d38372641953e69dfc2754b5bf0e
Wchida <wellingtonchida@protonmail.com>
2023-05-07 19:58:24 -0300

    ✨ feat: example factory db

---
+++ FactoryConnection/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FactoryConnection.Repositories;
+using System.Data;
+
+Console.WriteLine("Hello, World!");
+
+IDbConnection connection = new ConnectionFactory().GetConnection();
---
+++ FactoryConnection/Repositories/ConnectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace FactoryConnection.Repositories
+{
+    public class ConnectionFactory
+    {
+
+
+        public IDbConnection GetConnection()
+        {
+            IDbConnection connection = new SqlConnection(/*ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString*/);
+            connection.ConnectionString = "";
+            connection.Open();
+            return connection;
+        }
+    }
+}
